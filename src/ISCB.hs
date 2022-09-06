2e8975758e8c04ec61930f4a142828453e6c1862
Pavan Rikhi <pavan.rikhi@gmail.com>
2022-09-06 18:27:51 -0400

    Refactor Message Handling
    Define each message handler directly below it's Message type. Modify
    `makeMessageHandler` so that _it_ is responsible for handling the `Left`
    case when decoding instead of the specific message handler.

--- src/ISCB.hs
+++ src/ISCB.hs
@@ -44,14 +44,36 @@ import qualified Data.ByteString.Char8         as BC
 import qualified Data.ByteString.Lazy          as LBS
 
 
+-- MESSAGES
+
 -- | One distinct grouping of messages we can send & receive.
 data ISCBPing1Message = SendPing1
     deriving (Show, Read, Eq, Ord, Generic, ToJSON, FromJSON)
+
+ping1Handler :: RedisChannel -> ISCBPing1Message -> IO ()
+ping1Handler channel = \case
+    SendPing1 -> do
+        formattedTime <- getFormattedTime
+        putStrLn $ concat [formattedTime, "[", BC.unpack channel, "] Pong 1"]
 
 -- | A separate group of messages. Groups may be helpful for separating out
 -- the usecases of different inter-server communications.
 data ISCBPing2Message = SendPing2
     deriving (Show, Read, Eq, Ord, Generic, ToJSON, FromJSON)
+
+ping2Handler :: RedisChannel -> ISCBPing2Message -> IO ()
+ping2Handler channel = \case
+    SendPing2 -> do
+        formattedTime <- getFormattedTime
+        putStrLn $ concat [formattedTime, "[", BC.unpack channel, "] Pong 2"]
+
+-- | Handle messages that come through a subscription. This is used as
+-- initial subscription list when starting up the pubsub listener.
+messageHandlers :: [(RedisChannel, MessageCallback)]
+messageHandlers = [mkPing1Handler ping1Handler, mkPing2Handler ping2Handler]
+
+
+-- CHANNELS
 
 -- | All channels we can send & receive from.
 data ISCBChannel
@@ -75,36 +97,8 @@ instance MessageToChannel ISCBPing1Message where
 instance MessageToChannel ISCBPing2Message where
     toChannel = Ping2Channel
 
--- | Our handlers. You'd probably wanna define each somewhere that makes
--- more sense?
---
--- If we decide some standardized error message handling like below, we can
--- handle the `Left` case in 'makeMessageHandler'(with 'channelName
--- . toChannel' to make the error message) and only have to case match the
--- messages instead of a 'Either String msg'.
-messageHandlers :: [(RedisChannel, MessageCallback)]
-messageHandlers =
-    [ mkPing1Handler $ \channel -> \case
-        Left e ->
-            putStrLn $ "Could not decode message from `ping1` channel: " <> e
-        Right r -> case r of
-            SendPing1 -> do
-                formattedTime <- getFormattedTime
-                putStrLn $ concat
-                    [formattedTime, "[", BC.unpack channel, "] Pong 1"]
-    , mkPing2Handler $ \channel -> \case
-        Left e ->
-            putStrLn $ "Could not decode message from `ping2` channel: " <> e
-        Right r -> case r of
-            SendPing2 -> do
-                formattedTime <- getFormattedTime
-                putStrLn $ concat
-                    [formattedTime, "[", BC.unpack channel, "] Pong 2"]
-    ]
-  where
-    getFormattedTime :: IO String
-    getFormattedTime =
-        formatTime defaultTimeLocale "[%H:%M:%S] " <$> getCurrentTime
+
+-- INFRASTRUCTURE
 
 -- | Publish a message, routing it to the channel prescribed to it in it's
 -- 'MessageToChannel' instance.
@@ -140,13 +134,13 @@ withRedisSubs nextTo = do
 
 -- | Not really necessary, but gives us a little documentation?
 mkPing1Handler
-    :: (RedisChannel -> Either String ISCBPing1Message -> IO ())
+    :: (RedisChannel -> ISCBPing1Message -> IO ())
     -> (RedisChannel, MessageCallback)
 mkPing1Handler = makeMessageHandler
 
 -- | Another helper we may find unnecessary.
 mkPing2Handler
-    :: (RedisChannel -> Either String ISCBPing2Message -> IO ())
+    :: (RedisChannel -> ISCBPing2Message -> IO ())
     -> (RedisChannel, MessageCallback)
 mkPing2Handler = makeMessageHandler
 
@@ -155,8 +149,27 @@ mkPing2Handler = makeMessageHandler
 makeMessageHandler
     :: forall msg
      . (FromJSON msg, MessageToChannel msg)
-    => (RedisChannel -> Either String msg -> IO ())
+    => (RedisChannel -> msg -> IO ())
     -> (RedisChannel, MessageCallback)
 makeMessageHandler handler =
     let channel = channelName $ toChannel @msg
-    in  (channel, handler channel . eitherDecode' . LBS.fromStrict)
+    in  ( channel
+        , either
+                (\e ->
+                    putStrLn
+                        $  "Could not decode message from `"
+                        <> BC.unpack channel
+                        <> "` channel: "
+                        <> e
+                )
+                (handler channel)
+            . eitherDecode'
+            . LBS.fromStrict
+        )
+
+
+-- MISC
+
+getFormattedTime :: IO String
+getFormattedTime =
+    formatTime defaultTimeLocale "[%H:%M:%S] " <$> getCurrentTime
