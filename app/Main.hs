9d547e308f969d60c9a01909df6c8fb959a1de18
Pavan Rikhi <pavan.rikhi@gmail.com>
2022-09-07 13:29:27 -0400

    Add Request Logging to Web Server

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
 import           Network.Wai.Handler.Warp       ( runEnv )
+import           Network.Wai.Middleware.RequestLogger
+                                                ( logStdoutDev )
 
 import           HttpServer
 import           ISCB
 
 main :: IO ()
-main = withRedisSubs (runEnv 9001 . app)
+main = withRedisSubs (runEnv 9001 . logStdoutDev . app)
